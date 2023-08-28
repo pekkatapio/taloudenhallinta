eb02750b130d977eb1b04f241501a661af8cc432
Pekka Tapio Aalto <Pekka.Aalto@sasky.fi>
2023-08-28 10:57:59 +0300

    lisää Menu-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import styles from './App.module.scss'
 import Header from '../Header'
 import Item from '../Item'
+import Menu from '../Menu'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Item />
         <Item />
         <Item />
+        <Menu />
       </div>
     </>
   )
