95d4bcb3d52eec46de0a821b43aa5a8abaadb048
Pekka Tapio Aalto <pekka.aalto@sasky.fi>
2023-10-05 14:16:59 +0300

    lisää EditItem-komponentin, reitityksen ja tietojen välityksen

---
+++ src/components/EditItem/EditItem.jsx
@@ -0,0 +1,18 @@
+import { useLoaderData } from 'react-router-dom'
+import styles from './EditItem.module.scss'
+import ItemForm from '../ItemForm/ItemForm'
+
+function EditItem(props) {
+
+  const data = useLoaderData()
+
+  return (
+    <div className={styles.edititem}>
+      <h2>Merkinnän muokkaaminen</h2>
+      <ItemForm onItemSubmit={props.onItemSubmit} formData={data.item} />
+    </div>
+  )
+
+}
+
+export default EditItem
