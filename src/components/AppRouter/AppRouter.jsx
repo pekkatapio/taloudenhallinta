505209db9b3183bbb4b4db3ae6876d1a34c87416
Pekka Tapio Aalto <Pekka.Aalto@sasky.fi>
2023-10-11 10:10:26 +0300

    lisää kulutyypin lisäyksen

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -34,7 +34,8 @@ function AppRouter(props) {
           } },
         { path: "stats", element: <Stats /> },
         { path: "settings",
-          element: <Settings typelist={props.typelist} /> }
+          element: <Settings typelist={props.typelist}
+                             onTypeSubmit={props.onTypeSubmit} /> }
       ]
     }
   ])
